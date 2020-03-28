912b253b4dfd1fdb9fc55ecfa38b3be10f9bd180
my7h <ustoev@front.ru>
2020-03-28 04:06:31 +0300

    wds proxy + msleep

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,37 @@ const PATHS = {
 
 };
 
+/**
+ * Задержка выполнения треда на опред. число миллисекунд
+ * см. https://medium.com/@frontman/node-js-sleep-usleep-msleep-bba767557c20
+ * @param milliSeconds
+ * @returns {"ok" | "not-equal" | "timed-out"}
+ */
+const msleep = milliSeconds =>
+    Atomics.wait(
+        new Int32Array(new SharedArrayBuffer(4)),
+        0, 0, milliSeconds
+    )
+;
+
 module.exports = {
     entry: PATHS.source + '/index.js',
     output: {
         path: PATHS.build,
         filename: '[name].js'
+    },
+    devServer: { //https://github.com/webpack/webpack-dev-server/tree/master/examples/general
+        proxy: {
+            '/api/bypass-example': {
+                bypass: (req, res) => {
+                    console.log("proxy sleeping...");
+                    msleep(2000);
+                    console.log("proxy send response");
+                    return res.send({
+                    mssg: 'proxy server - Message came from bypass property in webpack'
+                })},
+            },
+        },
     },
     plugins: [
         new HtmlWebpackPlugin({
